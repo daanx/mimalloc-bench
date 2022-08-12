062b9d45a115dc39d961106a0fb33b5e4d58b45c
Matthew Parkinson <mattpark@microsoft.com>
2022-08-12 21:50:44 +0100

    Move the security benchmarks to use CMake.

--- bench.sh
+++ bench.sh
@@ -520,9 +520,8 @@ function run_test_env_cmd { # <test name> <allocator name> <environment args> <c
        ;;
     security)
        echo $2 >> $outfile
-       for file in security/*.c
+       for binary in security/*
        do
-          binary=${file%.*}
           tmpfile="$1-$2-tmp.txt"
           (/usr/bin/env $3 ./$binary || echo CRASHED ) 2>/dev/null > "$tmpfile"
           if grep --text -q 'NOT_CAUGHT' "$tmpfile"; then
