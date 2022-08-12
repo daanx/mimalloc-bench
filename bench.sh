5e1508f0250107829a19e9d95c225b2728a0b601
Matthew Parkinson <mattpark@microsoft.com>
2022-08-12 15:00:57 +0100

    Give a status of detected during teardown
    An allocator might determine corruption has occurred, but this might
    happen after the corruption.  This adds a new `t` status for when
    the allocator detects the problem at teardown, but not immediately `+`.

--- bench.sh
+++ bench.sh
@@ -523,10 +523,16 @@ function run_test_env_cmd { # <test name> <allocator name> <environment args> <c
        for file in security/*.c
        do
           binary=${file%.*}
-          if /usr/bin/env $3 ./$binary 2>/dev/null | grep --text -q 'NOT_CAUGHT'; then
-                  echo "[-] $binary" >> "$outfile"
+          tmpfile="$1-$2-tmp.txt"
+          (/usr/bin/env $3 ./$binary || echo CRASHED ) 2>/dev/null > "$tmpfile"
+          if grep --text -q 'CRASHED' "$tmpfile"; then
+            if grep --text -q 'NOT_CAUGHT' "$tmpfile"; then
+              echo "[t] $binary" >> "$outfile"
+            else
+              echo "[+] $binary" >> "$outfile"
+            fi
           else
-                  echo "[+] $binary" >> "$outfile"
+            echo "[-] $binary" >> "$outfile"
           fi
        done
        ;;
