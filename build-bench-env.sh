35483d77cb4b7525253b3991f3fcf7abb2d70df4
jvoisin <julien.voisin@dustri.org>
2022-08-16 19:47:44 +0200

    Take even less space when using DieHard

--- build-bench-env.sh
+++ build-bench-env.sh
@@ -509,7 +509,7 @@ fi
 if test "$setup_dh" = "1"; then
   checkout dh $version_dh dh https://github.com/emeryberger/DieHard
   # remove all the historical useless junk
-  rm -rf ./benchmarks/ ./src/archipelago/ ./src/build/ ./src/exterminator/ ./src/local/ ./src/original-diehard/ ./src/replicated/
+  rm -rf ./benchmarks/ ./src/archipelago/ ./src/build/ ./src/exterminator/ ./src/local/ ./src/original-diehard/ ./src/replicated/ ./docs
   if test "$darwin" = "1"; then
     TARGET=libdieharder make -C src macos
   else
