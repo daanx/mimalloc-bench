7bb2229f74e618f816493bed2f85472483279d93
Max Shavrick <mxms@google.com>
2022-08-20 00:38:24 +0000

    Fill chunks when allocating them, to see if they're cleared after free.

--- bench/security/zero_on_malloc.c
+++ bench/security/zero_on_malloc.c
@@ -4,7 +4,22 @@
 
 #include "common.h"
 
+#define ITERATIONS 32
+
 int main(void) {
+    // Allocate many chunks, and fill them.
+    // Do this incase the allocator randomizes new chunks
+    char *allocations[ITERATIONS] = { 0 };
+    for (int i = 0; i < ITERATIONS; i++) {
+        char *chunk = malloc(ALLOCATION_SIZE);
+        memset(chunk, 'A', ALLOCATION_SIZE);
+        allocations[i] = chunk;
+    }
+
+    for (int i = 0; i < ITERATIONS; i++) {
+        free(allocations[i]);
+    }
+
     char *p = malloc(ALLOCATION_SIZE);
     for (int i=0; i< ALLOCATION_SIZE; i++) {
         if (p[i] != 0) {
