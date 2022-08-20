a6daaf94af396257e94b666bd5e13a9ecbed0df1
jvoisin <julien.voisin@dustri.org>
2022-08-20 14:04:59 +0200

    Add memcpy underflow tests

---
+++ bench/security/one_mbyte_memcpy_underflow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+// This test checks for things like https://github.com/microsoft/snmalloc/blob/main/docs/security/GuardedMemcpy.md
+
+int main(void) {
+    const char c[ALLOCATION_SIZE] = {0};
+    char *p = malloc(ALLOCATION_SIZE);
+    memcpy(p - 1024 * 1024, c, sizeof(c));
+
+    NOT_CAUGHT();
+
+    return 0;
+}
