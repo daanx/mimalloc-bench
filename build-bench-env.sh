5cf7ca38c46086416789c66319cc8b38457eb4a4
Stephan Bauroth <s.bauroth@tu-berlin.de>
2025-06-27 13:54:31 +0200

    build: rocksdb: do not include TCMalloc

--- build-bench-env.sh
+++ build-bench-env.sh
@@ -772,7 +772,7 @@ if test "$setup_rocksdb" = "1"; then
   fi
 
   cd "rocksdb-$version_rocksdb"
-  DISABLE_WARNING_AS_ERROR=1 DISABLE_JEMALLOC=1 make db_bench -j $procs
+  DISABLE_WARNING_AS_ERROR=1 DISABLE_JEMALLOC=1 ROCKSDB_DISABLE_TCMALLOC=1 make db_bench -j $procs
   [ "$CI" ] && find . -name '*.o' -delete
   popd
 fi
