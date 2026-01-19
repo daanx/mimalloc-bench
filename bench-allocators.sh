0d226fab3d3e1d624f10d1ab351d89676b51a176
Zsomething <nothanks@ontheemail.com>
2026-01-18 22:39:11 -0700

    include more information about the host in the bench results file to improve reproducibility

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -1,14 +1,49 @@
-echo CPU type:
+# CPU type on linuxy
+CPUTYPE=`grep "model name" /proc/cpuinfo 2>/dev/null | uniq | cut -d':' -f2-`
 
-# print out CPU type on macos
-sysctl -n machdep.cpu.brand_string 2>/dev/null
+if [ "x${CPUTYPE}" = "x" ] ; then
+    # CPU type on macos
+    CPUTYPE=`sysctl -n machdep.cpu.brand_string 2>/dev/null`
+fi
 
-# print out CPU type on linux
-grep "model name" /proc/cpuinfo 2>/dev/null | uniq
+CPUTYPE="${CPUTYPE//[^[:alnum:]]/}"
 
-ALLOCATORS="je mi2 rp sn s"
+OSTYPESTR="${OSTYPE//[^[:alnum:]]/}"
 
-BUILD_TESTS="rocksdb linux"
+ARGS=$*
+ARGSSTR="${ARGS//[^[:alnum:]]/}"
+
+BNAME="mimalloc-bench"
+FNAME="${BNAME}.result.${CPUTYPE}.${OSTYPESTR}.${ARGSSTR}.txt"
+RESF="tmp/${FNAME}"
+
+echo "# Saving result into \"${RESF}\""
+
+rm -f $RESF
+mkdir -p tmp
+
+echo "# git log -1 | head -1" 2>&1 | tee -a $RESF
+git log -1 | head -1 2>&1 | tee -a $RESF
+echo 2>&1 | tee -a $RESF
+
+echo "( [ -z \"\$(git status --porcelain)\" ] && echo \"Clean\" || echo \"Uncommitted changes\" )" 2>&1 | tee -a $RESF
+( [ -z "$(git status --porcelain)" ] && echo "Clean" || echo "Uncommitted changes" ) 2>&1 | tee -a $RESF
+echo 2>&1 | tee -a $RESF
+
+echo CPU type: 2>&1 | tee -a $RESF
+echo $CPUTYPE 2>&1 | tee -a $RESF
+echo 2>&1 | tee -a $RESF
+
+echo OS type: 2>&1 | tee -a $RESF
+echo $OSTYPE 2>&1 | tee -a $RESF
+echo 2>&1 | tee -a $RESF
+
+if [ "x${OSTYPE}" = "xmsys" ]; then
+    # No jemalloc or snmalloc on windows
+    ALLOCATORS="mi2 rp s"
+else
+    ALLOCATORS="je sn mi2 rp s"
+fi
 
 # I picked these ones because I imagine they are more representative of real workloads than the
 # other benchmarks in here.
@@ -16,8 +51,11 @@ BENCHES="gs leanN lua"
 
 EXTRA_BENCHES="rocksdb linux"
 
-./build-bench-env.sh ${ALLOCATORS} packages bench ${EXTRA_BENCHES}
+./build-bench-env.sh ${ALLOCATORS} packages bench ${EXTRA_BENCHES} &&
 
-cd out/bench
+mkdir out/bench &&
 
-../../bench.sh ${ALLOCATORS} ${BENCHES} ${EXTRA_BENCHES}
+(
+    cd out/bench &&
+    ../../bench.sh ${ALLOCATORS} ${BENCHES} ${EXTRA_BENCHES}
+) 2>&1 | tee $RESF
